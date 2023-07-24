590ad88f2f9ef285ffa39c31541a490c8a9c9e8d
HichamZineb <hichamelamine@hotmail.com>
2023-07-24 08:50:45 -0700

    update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,23 +10,25 @@
 
 void selection_sort(int *array, size_t size)
 {
-  unsigned int i, j, min, tmp;
+	unsigned int i, j, min, tmp;
 
-  for (i = 0; i < size; i++)
-  {
-    min = i;
+	for (i = 0; i < size; i++)
+	{
+		min = i;
 
-    for (j = i + 1; j < size; j++)
-    {
-      if (array[j] < array[min])
-        min = j;
-    }
-    if (min != i)
-    {
-      tmp = array[i];
-      array[i] = array[min];
-      array[min] = tmp;
-    }
-    print_array(array, size);
-  }
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+
+		if (min != i)
+		{
+			tmp = array[i];
+			array[i] = array[min];
+			array[min] = tmp;
+		}
+
+		print_array(array, size);
+	}
 }
